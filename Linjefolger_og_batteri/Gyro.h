a258b3face23fc99bf0f581092bae5da0ec71a12
EndreKalheim <101863036+EndreKalheim@users.noreply.github.com>
2022-05-05 09:22:58 +0200

    Update Gyro.h

--- Linjefolger_og_batteri/Gyro.h
+++ Linjefolger_og_batteri/Gyro.h
@@ -5,6 +5,7 @@
 
 
 
+
 //Globale variabler gyroskop:
 int gyro_r; //definerer en variabel
 
@@ -13,15 +14,17 @@ int gyro_r; //definerer en variabel
 
 void Battery_reset() { //void funksjon som leser av akselerometeret 
   gyro.read(); //leser akselereometeret 
-  gyro_r = gyro.g.y;
-  if (gyro_r > 800 && balance >= 200) { //oppdager at den blir snudd 
+  gyro_r = gyro.g.y; 
+  if (gyro_r > 800 && balance >= 200) { //Sjekker om bilen blir snudd og om bilen har nok penger til å bytte batteri 
     EEPROM.update(0, 100); //oppdaterer batteriets kvaitet. Kanskje legge til en buzzer lys for å si at den er klar med en vent på button press. Kanskje sette batteri verdien tilbake til 1200.
-    low_power();
-    zumodrift = 8;
-    kapasitet = 1200;
-    Serial1.println(balance + 1200);
-    Serial1.println(kapasitet + 100);
-    buttonA.waitForButton();
+    low_power(); //lager lyd funksjon
+    zumodrift = 8; //Setter bilen i fra hjem mode
+    kapasitet = 1200; //Setter batteriet til maks
+    balance = balance + 1200; //Trekker fra hvor mye penger som er brukt og gjør det klar for sending ved å legge til 1400 sånn at esp-en skjønner at dette er en penge verdi
+    delay(2000); //venter litt
+    Serial1.println(kapasitet + 100); //sender den nye veriden på hvor mye strøm som er igjen på bilen.
+    Chargetime = 1;   //bryter while loopen
+    buttonA.waitForButton();  //venter på at butten A skal bli presset sånn at bilen ikke gjører før brukeren er klar
   }
 }
 
